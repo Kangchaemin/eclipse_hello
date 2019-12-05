22b2cd2695fae0048696cbc2524eaf25f8ee8daa
Kangchaemin <43837994+Kangchaemin@users.noreply.github.com>
2019-12-05 12:40:21 +0900

    add print line

--- src/se/Main.java
+++ src/se/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World2");
+		System.out.println("Hello World3");
+		System.out.println("1: branch hotfix");
 	}
 
 }
