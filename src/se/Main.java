aa623e612978329287295ea903eef420b7db400f
Kangchaemin <43837994+Kangchaemin@users.noreply.github.com>
2019-12-05 12:46:15 +0900

    xxx

--- src/se/Main.java
+++ src/se/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World3");
+		System.out.println("Hello World");
 		System.out.println("1: branch hotfix");
 	}
 
